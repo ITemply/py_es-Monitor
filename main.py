3f0ebc3c3b0e280063dfad9f5141b87b138a98fb
I Temply <bluerambles278@gmail.com>
2024-04-27 14:20:12 +0000

    Sync, Env

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import requests
 import cv2
 import easyocr
+import os 
+import webbrowser
 
-esPlace = 2262441883
+from dotenv import load_dotenv
+load_dotenv()
+
+esPlace = os.environ['PLACE_ID']
+print(esPlace)
 
 def getJobs(placeId):
   newRequest = requests.request('GET', f'https://games.roblox.com/v1/games/{str(placeId)}/servers/0?sortOrder=2&excludeFullGames=false&limit=100')
@@ -12,6 +18,8 @@ def scanImage(pathTo):
   img = cv2.imread(pathTo)
   reader = easyocr.Reader(['en'], gpu=False)
   text = reader.readtext(img)
-  print(text)
+  return text
 
-scanImage('images/image.png')
+def createJoinCode(placeId, jobId):
+  formattedString = f'roblox://experiences/start?placeId={placeId}&gameInstanceId={jobId}'
+  return formattedString
